8687c74343a16ecf37c050d4ed8185ce859becda
filecoion <filecoion@protonmail.com>
2020-09-04 19:36:19 +0800

    完成基本界面

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 $(document).ready(function () {
    var addbutton=$("#add_button");
-   addbutton.on("click",function () {
+   addbutton.on("mousedown",function () {
        var msg=prompt("请输入或粘贴你的ETH钱包地址");
        alert(msg);
+   });
+
+    var clipboard = new Clipboard('#copyaddr');
+
+    clipboard.on('success', function (e) {
+        alert("复制成功，请粘贴到你的QQ/微信上推荐给你的好友");
+    });
+
+    clipboard.on('error', function (e) {
+        alert(e);
+    });
+
+    var copybutton=$("#copyaddr");
+    copybutton.on("mousedown",function () {
+        $(this).attr("data-clipboard-text",$("#ethaddr").text());
    })
 });
